90cde391452889f14ca92f611d14b3497fdb397a
mali-mrav <59232363+mali-mrav@users.noreply.github.com>
2020-05-25 00:45:37 +0200

    Add files via upload

--- slide.js
+++ slide.js
@@ -30,5 +30,18 @@ pauseButton.onclick = function(){
 
 
 
-/* IPOD JE SKRIPTA ZA LINK POSLANICI */
+/* IPOD JE SKRIPTA ZA POZADINU */
 
+var els = document.querySelectorAll('.vh-fix')
+if (!els.length) return
+
+for (var i = 0; i < els.length; i++) {
+  var el = els[i]
+  if (el.nodeName === 'IMG') {
+    el.onload = function() {
+      this.style.height = this.clientHeight + 'px'
+    }
+  } else {
+    el.style.height = el.clientHeight + 'px'
+  }
+}
